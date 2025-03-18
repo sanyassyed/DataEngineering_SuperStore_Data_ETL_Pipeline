b718dbd7ec5a03ddd2d02255a04414cdba0c3ecb
ubuntu <ubuntu@superstore.com>
2025-03-18 16:38:24 +0000

    ADD: Updated comments

--- script/run.py
+++ script/run.py
@@ -10,8 +10,8 @@ from aws_utils.aws_utils import connect_to_s3
 
 load_dotenv()
 
-# Loading env variables
-# Database secrets
+# Load environment variables
+# Database credentials
 HOST_MYSQL = os.getenv('HOST_MYSQL')
 USER = os.getenv('USER_MYSQL')
 PASSWORD = os.getenv('PASSWORD')
@@ -20,7 +20,7 @@ PASSWORD = os.getenv('PASSWORD')
 LOG_FILE = os.getenv('LOG_FILE_PYTHON')
 OUTPUT_FOLDER = os.getenv('OUTPUT_FOLDER')
 
-# Setting up logging
+# Configure logging
 logging.basicConfig(
                     level=logging.DEBUG,
                     format="%(asctime)s %(levelname)s : %(message)s",
@@ -28,149 +28,145 @@ logging.basicConfig(
                     filename=LOG_FILE,
                    )
 
-logging.info(f"LOG FILE FOR THIS PYTHON SCRIPT IS AT: {LOG_FILE}")
+logging.info(f"Log file for this script: {LOG_FILE}")
 
 
 def connect_db(db_name):
     """
-    Connects to the DB db_name and returns the connection engine
+    Establishes a connection to the specified database and returns the connection engine.
     
     Args:
-    db_name (str): The name of the database to connect to on AWS RDS
+    db_name (str): Name of the database hosted on AWS RDS.
 
     Returns:
-    sqlalchemy.engine.base.Engine: Connects to the db and returns the connection engine
-
+    sqlalchemy.engine.base.Engine: Database connection engine.
     """
     connection_string = f"mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST_MYSQL}:3306/{db_name}"
     try:
         engine = create_engine(connection_string, echo=True)
-        logging.info(f"Connected to the Database")
+        logging.info("Successfully connected to the database.")
     except Exception as e:
-        logging.error(f"Something went wrong: {e}")
-        logging.error("Could not connect to the database")
+        logging.error(f"Database connection failed: {e}")
         return None
-    # print(f"This is what is returned: {type(engine)}")
     return engine
 
 def disconnect_db(engine):
     """
-    Disconnects from the db using the connection engine
+    Closes the database connection.
 
     Args:
-    engine (sqlalchemy.engine.base.Engine): The connection engine connected to the db
-
+    engine (sqlalchemy.engine.base.Engine): Active database connection engine.
     """
     engine.dispose()
-    logging.info("Database connection closed")
+    logging.info("Database connection closed.")
     return None
 
 def extract(engine, output_file_name):
     """
-    Uses the connection engine and extracts query result from the database using pandas 
-    And stores the df as json data with output_file_name in the output folder
+    Executes a query to retrieve the top 10 customers based on total sales, 
+    saves the results as a JSON file in the output directory.
 
     Args:
-    engine (sqlalchemy.engine.base.Engine): the connection engine with the db connection
-    output_file_name (str): The name for the .json data file
+    engine (sqlalchemy.engine.base.Engine): Active database connection engine.
+    output_file_name (str): Filename for the output JSON file.
 
     Returns:
-    str: The path along with the name of the .json file where the output data is stored
+    str: Path of the saved JSON file.
     """
-    logging.info("Querying the orders table")
+    logging.info("Executing query on the orders table.")
     output_file = os.path.join(OUTPUT_FOLDER, output_file_name)
     if not os.path.exists(OUTPUT_FOLDER):
-            logging.info(f"Creating output folder at: {OUTPUT_FOLDER}")
+            logging.info(f"Creating output directory: {OUTPUT_FOLDER}")
             os.mkdir(OUTPUT_FOLDER)
 
-    query = f"SELECT CustomerID, SUM(Sales) TotalCustomerSales  FROM orders GROUP BY CustomerID ORDER BY 2 DESC LIMIT 10"
+    query = """SELECT CustomerID, SUM(Sales) AS TotalCustomerSales  
+               FROM orders 
+               GROUP BY CustomerID 
+               ORDER BY TotalCustomerSales DESC 
+               LIMIT 10"""
     
     try:
         df = pd.read_sql(query, con=engine)
         df.to_json(output_file, index=False)
-        logging.info(f"Data extracted from db & written to the file {output_file}")
+        logging.info(f"Data successfully extracted and saved to {output_file}.")
         return output_file
     except pd.errors.DatabaseError as e:
-        logging.error(f"Following error when running query on the database: {e}")
+        logging.error(f"Database query error: {e}")
     except Exception as e:
-        logging.error(f"Following error when running query on the database: {e}")
+        logging.error(f"Unexpected error during query execution: {e}")
     
     return None
 
 
 def save_to_s3(output_file_name : str, output_file_path: str, bucket_name : str, region : str ='us-east-2') -> None:
     """
-    Uploads a DataFrame to an S3 bucket as a Parquet file. Creates the S3 bucket if it doesn't exist.
+    Uploads the extracted JSON data file to an S3 bucket. If the bucket does not exist, it will be created.
     
     Args:
-        df (pandas.DataFrame): The DataFrame to upload.
-        output_file_name (str): The name of the file to be saved in S3.
-        bucket_name (str): The name of the S3 bucket.
-        region (str): The AWS region where the bucket is located (default: 'us-east-2').
+    output_file_name (str): Name of the file to be stored in S3.
+    output_file_path (str): Local path of the file to be uploaded.
+    bucket_name (str): Name of the target S3 bucket.
+    region (str): AWS region where the bucket is located (default: 'us-east-2').
     """
-    # Establish a connection to AWS S3 
+    # Establish connection to AWS S3 
     s3, s3_client = connect_to_s3()
     output_file_name_s3 = f'input/{output_file_name}'
 
     if s3 and s3_client:
-        # Check & create s3 bucket
+        # Check if the bucket exists; if not, create it.
         if not s3.Bucket(bucket_name) in s3.buckets.all():
-            logging.info(f"{bucket_name} BUCKET DOES NOT EXIST IN S3; SO CREATING IT")
+            logging.info(f"Bucket '{bucket_name}' does not exist. Creating it...")
             s3_client.create_bucket(
                                     Bucket=bucket_name,
-                                    CreateBucketConfiguration={'LocationConstraint':region}
+                                    CreateBucketConfiguration={'LocationConstraint': region}
                                     )
-            logging.info(f"{bucket_name} BUCKET CREATED ON S3")
+            logging.info(f"Bucket '{bucket_name}' successfully created on S3.")
 
-
-        logging.info(f"Uploading data to S3 bucket: {bucket_name} in region: {region}")
+        logging.info(f"Uploading {output_file_name} to S3 bucket: {bucket_name} in region: {region}.")
 
         try:
             with open(output_file_path, "rb") as file_data: 
                 s3_client.put_object(Bucket=bucket_name, Key=output_file_name_s3, Body=file_data)
-            logging.info(f"{output_file_name} DATA UPLOADED TO S3 AS PARQUET")
+            logging.info(f"{output_file_name} successfully uploaded to S3.")
         except Exception as e:
-            logging.error(f"DATA UPLOAD FAILED DUE TO: {e}")
+            logging.error(f"S3 upload failed: {e}")
         return
     else:
-        logging.error("Could not connect to S3")
+        logging.error("Failed to establish connection to S3.")
         return
 
 
 def main():
     """
-    Connects to superstore db on AWS RDS
-    Pulls Top 10 customer data via query
-    Store the results as json file locally in the output folder
-    Uploads the json file to s3 bucket
-
+    Runs the ETL process:
+    - Connects to the superstore database on AWS RDS.
+    - Extracts the top 10 customers based on total sales and saves results locally.
+    - Uploads the extracted data to an S3 bucket.
     """
-    # Extracting credentials from the config file
-    # Database name
+    # Load database and AWS configurations from a config file
     app_config = toml.load('config.toml')
     db_name = app_config['mysql']['database']
 
-    # AWS config
+    # AWS configuration
     aws_bucket_name = app_config['aws']['bucket_name']
     aws_region = app_config['aws']['region']
 
-    # Setting json file name
+    # Generate filename for the extracted JSON file
     output_file_name = f"top_10_customers_{time.strftime('%Y%m%d-%H%M%S')}.json"
 
-    # Connecting to DB
+    # Establish database connection
     engine = connect_db(db_name)
     
     if engine is not None:
-        # Extracting data from DB and saving locally as a json file
+        # Extract data from the database and save locally
         output_file_path = extract(engine, output_file_name)
         disconnect_db(engine)
 
-        # Upload to S3
-        # Uploading the json file to s3
+        # Upload the extracted data to S3
         save_to_s3(output_file_name, output_file_path, aws_bucket_name, aws_region)
-        logging.info(f"ETL Complete!")
+        logging.info("ETL process completed successfully.")
     else:
-        logging.error("Could not connect to the Database, aborting !!")
+        logging.error("Database connection failed. ETL process aborted.")
 
 if __name__=="__main__":
     main()
